4ca1cdf88c35ea798e915710e123a286b663f0cd
Bangkokian <jroven@gmail.com>
2024-04-03 15:59:46 +0700

    Update populate-from-cms.js
    Pin items

--- populate-from-cms.js
+++ populate-from-cms.js
@@ -75,7 +75,49 @@ function writeSolutionsServices() {
     });
 
 
-    var hiddenSourceItems = hiddenSource.querySelectorAll('div[role="listitem"]');
+    //var hiddenSourceItems = hiddenSource.querySelectorAll('div[role="listitem"]');
+
+    // ALLOW PINNED ITEMS
+    // SORT THE ITEMS IN THE ITEMS BY THE VALUE OF .pin-order (which is 1, 2 or 3)
+    // Note: If there are duplicate numbers in the Fraia CMS, it will pin them all. eg: 1,1,2,3,3
+
+
+     var hiddenSourceItems = Array.from(hiddenSource.querySelectorAll('div[role="listitem"]'));
+
+        hiddenSourceItems.sort(function(a, b) {
+          // Sorting logic remains the same
+          const aValue = a.querySelector('.pin-order').innerText;
+          const bValue = b.querySelector('.pin-order').innerText;
+
+          const aNum = parseInt(aValue, 10);
+          const bNum = parseInt(bValue, 10);
+
+          if ([1, 2, 3].includes(aNum) && [1, 2, 3].includes(bNum)) {
+            return aNum - bNum;
+          } else if ([1, 2, 3].includes(aNum)) {
+            return -1;
+          } else if ([1, 2, 3].includes(bNum)) {
+            return 1;
+          }
+          return 0;
+        });
+
+        // Re-insert sorted elements into their parent container
+        hiddenSourceItems.forEach(function(item) {
+          // This moves each item to the end of the parent container, effectively sorting them
+          hiddenSource.appendChild(item);
+        });
+
+        // If you need a NodeList of the sorted items for further operations
+        var hiddenSourceSortedItems = hiddenSource.querySelectorAll('div[role="listitem"]');
+
+
+
+
+
+
+
+
 
     consoleLog('items length: '+hiddenSourceItems.length);
 
@@ -94,6 +136,7 @@ function writeSolutionsServices() {
         var extractSummary = hiddenSourceItems[i].querySelector('.hidden-summary').innerText; // Adjust the selector as needed
         var extractHref = hiddenSourceItems[i].querySelector('a').href; // Assuming the button is an <a> element
         var extractServicesSolutions =  hiddenSourceItems[i].querySelector('#service-solution').innerText; // Value is either "Services" or "Solutions"
+        var extractPinOrder =  hiddenSourceItems[i].querySelector('.pin-order').innerText; // Value is 1,2,3 (anything else is unordered)
 
         consoleLog(extractHeader);
         consoleLog(extractSummary);
